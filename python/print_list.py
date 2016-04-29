357fdbc4a9fc666720917ec3d811f0e23baeec6a
syntaxSizer <kakuma4never@gmail.com>
2016-04-29 15:16:08 +0300

    double values in the list

--- python/print_list.py
+++ python/print_list.py
@@ -8,3 +8,17 @@ def print_list(x):
         print x[i]
 #calling the function
 print_list(n)
+
+
+
+
+#modify list items
+def double_list(n):
+    #loop through the list items and double each item
+    for i in range(0, len(n)):
+        # double each item in the list and store the new value back in the list (override the old value)
+        n[i] = n[i] * 2
+# Don't forget to return your new list!
+    return n
+
+print double_list(n)
