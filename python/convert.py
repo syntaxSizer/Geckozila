1d8a492268de2dcd68f5d4ab3e46d70c5f2d5f1b
syntaxSizer <kakuma4never@gmail.com>
2016-05-01 23:55:11 +0300

    convert pound to kilo , inch to cm

---
+++ python/convert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+#user input goes here
+inchs=int(raw_input("Enter Inchs "))
+pounds=int(raw_input("Enter Pounds "))
+
+#def converter( inchs, pounds):
+
+cm=inchs/0.39370
+kg=pounds/2.2046
+print cm , " centimeters"
+print kg , " kilos"
+#calling the method
+#converter(inchs,pounds)
+
