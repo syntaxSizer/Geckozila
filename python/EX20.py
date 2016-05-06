a15bb4b7da6bdc66f039404f2d722833a8eae127
syntaxSizer <kakuma4never@gmail.com>
2016-05-06 09:05:42 +0300

    more founction

---
+++ python/EX20.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+    print f.read()
+
+def rewind(f):
+    print f.seek(0)
+
+def print_a_line(line_count,f):
+    print line_count, f.readline()
+
+current_file=open(input_file):
+
+print "First lets prints the whole file:\n"
+
+
+print_all(current_file)
+
+
+print "Now let's rewind, kind of like a tap."
+
+
+current_line=1
+prisent_a_line(current_line,current_file)
+
+
+current_line=1
+
