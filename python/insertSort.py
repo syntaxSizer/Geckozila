75587c307fefa74d70478f731c0585e4216ab5d8
syntaxSizer <kakuma4never@gmail.com>
2016-05-09 11:48:17 +0300

    insertion sort using python

---
+++ python/insertSort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+print "Enter 4 different number please"
+a=[int(raw_input('Enter number'))
+,int(raw_input('Enter number'))
+,int(raw_input('Enter number'))
+,int(raw_input('Enter number'))] 
+
+
+def insertionsort(a):
+    b=[]
+    c=0
+    for i in a:
+        for j in a[i]+1:
+            if(a[i]<a[j]):
+                c=
+          
+
+
