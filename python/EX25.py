ab071238fefa9b18b6aabecf1889e5ebcb525591
syntaxSizer <kakuma4never@gmail.com>
2016-05-10 13:08:16 +0300

    String built in functions

---
+++ python/EX25.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+def break_words(stuff):
+#This function gnna break up words for us
+    words = stuff.split(' ')
+    return words
+
+
+def sort_words(words)
+#this function gonna sort the word alphabticly
+    return sorted(words)
+
+
+
