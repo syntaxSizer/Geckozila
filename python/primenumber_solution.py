e7623413d1a4853003e56063b6020d4defead65b
syntaxSizer <kakuma4never@gmail.com>
2016-05-19 21:30:24 +0300

    add prime number toy problem

---
+++ python/primenumber_solution.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+
+y=int(raw_input('number yako')
+
+
+def prime_num(x):
+   if x%x == 0 and x % 1 == 0:
+       if x % 2 != 0:
+           return True
+       else:
+           return False
+
+prime_num(y)
